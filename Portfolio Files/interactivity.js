6375707d9316d674b0c2d1ed5107179979c02521
Johnny Hagedorn <johnny@169-231-19-83.wireless.ucsb.edu>
2025-01-28 20:11:49 -0800

    files

---
+++ Portfolio Files/interactivity.js
@@ -0,0 +1,8 @@
+// Example: Dark mode toggle
+const toggleButton = document.createElement('button');
+toggleButton.innerText = 'Toggle Dark Mode';
+document.body.prepend(toggleButton);
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
