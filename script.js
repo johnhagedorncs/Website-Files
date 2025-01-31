89c41cd4cd9fc740bc4581062cfe67bf79645d30
Johnny Hagedorn <johnny@169-231-22-254.wireless.ucsb.edu>
2025-01-30 20:23:51 -0800

    jan 30

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ document.body.prepend(toggleButton);
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });*/
+
+//All links are new tab (rather than replacement)
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('a[href^="http"]').forEach(link => {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+  });
+});
